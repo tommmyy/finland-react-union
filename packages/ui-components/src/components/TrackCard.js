aba2b5ede2f1889a61fcf42cb4e1b2491cc06e93
Tomáš Konrády <tomaskonrady@gmail.com>
2019-04-24 12:34:40 +0300

    onClickLike

--- packages/ui-components/src/components/TrackCard.js
+++ packages/ui-components/src/components/TrackCard.js
@@ -1,35 +1,43 @@
 import React from 'react';
-import { cx } from 'ramda-extension';
+import { cx, noop } from 'ramda-extension';
 import classes from './TrackCard.css';
 
-const TrackCard = ({
-	className,
-	children,
-	track: { id, name, artist, image, votes, order, spotifyHref },
-}) => (
-	<div className={cx(classes.card, className)} key={id}>
-		<div className={cx(classes.item, classes.order)}>{order}</div>
-		<div className={cx(classes.item, classes['image-wrapper'])}>
-			<img
-				className={classes.image} src={image} width="64"
-				height="64"
-			/>
+const TrackCard = ({ className, children, onClickLike, track }) => {
+	const { id, name, artist, image, votes, order, spotifyHref } = track;
+	return (
+		<div className={cx(classes.card, className)} key={id}>
+			<div className={cx(classes.item, classes.order)}>{order}</div>
+			<div className={cx(classes.item, classes['image-wrapper'])}>
+				<img
+					className={classes.image} src={image} width="64"
+					height="64"
+				/>
+			</div>
+			<div className={cx(classes.item, classes.votes)}>{votes}x</div>
+			<div className={cx(classes.item, classes['text-wrapper'])}>
+				<div className={cx(classes['track-name'])}>{name}</div>
+				<div className={cx(classes.artist)}>{artist}</div>
+			</div>
+			<div className={cx(classes.item, classes['actions-wrapper'])}>
+				<a className={cx(classes.action)} href={spotifyHref} target="_blank">
+					play
+				</a>
+				<span
+					className={cx(classes.action, classes['action-like'])}
+					onClick={event => {
+						onClickLike(event, track);
+					}}
+				>
+					like
+				</span>
+			</div>
+			{children}
 		</div>
-		<div className={cx(classes.item, classes.votes)}>{votes}x</div>
-		<div className={cx(classes.item, classes['text-wrapper'])}>
-			<div className={cx(classes['track-name'])}>{name}</div>
-			<div className={cx(classes.artist)}>{artist}</div>
-		</div>
-		<div className={cx(classes.item, classes['actions-wrapper'])}>
-			<a className={cx(classes.action)} href={spotifyHref} target="_blank">
-				play
-			</a>
-			<a className={cx(classes.action, classes['action-like'])} href={spotifyHref} target="_blank">
-				like
-			</a>
-		</div>
-		{children}
-	</div>
-);
+	);
+};
+
+TrackCard.defaultProps = {
+	onClickLike: noop,
+};
 
 export default TrackCard;
