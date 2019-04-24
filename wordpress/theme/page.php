1441a3244a962da1738f39702bbac2b749d50f64
Tomáš Konrády <tomaskonrady@gmail.com>
2019-04-24 17:04:47 +0300

    new wp theme

--- wordpress/theme/page.php
+++ wordpress/theme/page.php
@@ -1,19 +1,34 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package Neve
+ * @since   1.0.0
+ */
+$container_class = apply_filters( 'neve_container_class_filter', 'container', 'single-page' );
 
+get_header();
+
+?>
+<div class="<?php echo esc_attr( $container_class ); ?> single-page-container">
 	<div class="row">
-
-		<div class="col-sm-12">
-
-			<?php 
-				if ( have_posts() ) : while ( have_posts() ) : the_post();
-  	
-					get_template_part( 'content', get_post_format() );
-  
-				endwhile; endif; 
+		<?php do_action( 'neve_do_sidebar', 'single-page', 'left' ); ?>
+		<div class="nv-single-page-wrap col">
+			<?php
+			do_action( 'neve_page_header', 'single-page' );
+			do_action( 'neve_before_content', 'single-page' );
+			if ( have_posts() ) {
+				while ( have_posts() ) {
+					the_post();
+					get_template_part( 'template-parts/content', 'page' );
+				}
+			} else {
+				get_template_part( 'template-parts/content', 'none' );
+			}
+			do_action( 'neve_after_content', 'single-page' );
 			?>
-
-		</div> <!-- /.col -->
-
-	</div> <!-- /.row -->
-
+		</div>
+		<?php do_action( 'neve_do_sidebar', 'single-page', 'right' ); ?>
+	</div>
+</div>
 <?php get_footer(); ?>
--- wordpress/theme/single.php
+++ wordpress/theme/single.php
@@ -1,16 +1,34 @@
-<?php get_header(); ?>
-	<div class="row">
-		<div class="col-sm-12">
-			<?php 
-				if ( have_posts() ) : while ( have_posts() ) : the_post();
-					get_template_part( 'content-single', get_post_format() );
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				endwhile; endif; 
-			?>
-		</div> <!-- /.col -->
-	</div> <!-- /.row -->
-<?php get_footer(); ?>
+<?php
+/**
+ * Author:          Andrei Baicus <andrei@themeisle.com>
+ * Created on:      28/08/2018
+ *
+ * @package Neve
+ */
 
+$container_class = apply_filters( 'neve_container_class_filter', 'container', 'single-post' );
 
+get_header();
+
+?>
+	<div class="<?php echo esc_attr( $container_class ); ?> single-post-container">
+		<div class="row">
+			<?php do_action( 'neve_do_sidebar', 'single-post', 'left' ); ?>
+			<article id="post-<?php echo esc_attr( get_the_ID() ); ?>"
+					class="<?php echo esc_attr( join( ' ', get_post_class( 'nv-single-post-wrap col' ) ) ); ?>">
+				<?php
+				if ( have_posts() ) {
+					while ( have_posts() ) {
+						the_post();
+						get_template_part( 'template-parts/content', 'single' );
+					}
+				} else {
+					get_template_part( 'template-parts/content', 'none' );
+				}
+				?>
+			</article>
+			<?php do_action( 'neve_do_sidebar', 'single-post', 'right' ); ?>
+		</div>
+	</div>
+<?php
+get_footer();
