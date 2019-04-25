379d18684fac5c8afa7327ec5bf82548356fb748
Tomáš Konrády <tomaskonrady@gmail.com>
2019-04-26 00:26:14 +0300

    adding comments to speed up the demo

--- packages/app-finnish-metal/src/components/Root/Root.js
+++ packages/app-finnish-metal/src/components/Root/Root.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 
 import SongsOverview from '@finland/widget-songs-overview';
+import SongsRating from '@finland/widget-songs-rating';
 
 import './Root.css';
 
 const Root = () => <SongsOverview />;
+// const Root = () => <SongsRating />;
 
 export default hot(module)(Root);
